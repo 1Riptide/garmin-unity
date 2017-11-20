e989cfce9c26edd9a9db3bd32904f0567d371524
Casey Stalnaker <casey@caseystalnaker.com>
2017-11-20 06:08:21 -0600

    First Commit

---
+++ Assets/ShotDistroChart.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotDistroChart : MonoBehaviour {
+
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (addDataPoints ());
+	}
+
+	IEnumerator addDataPoints(){
+		int shotCount = 1490;
+		for (int i = 0; i < shotCount; i++) {
+			// x range is 8 thru -8
+			// y range is 14 thru -14
+			yield return new WaitForSeconds(Random.Range(0.01f, 0.04f));
+			addDataPoint(new Vector3(Random.Range(-8.0f, 8.0f), 0, Random.Range(-14.0f, 14.0f)));
+		}
+	}
+
+	IEnumerator addDataPoint(Vector3 location){
+		//yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
+		Instantiate(prefab, location, Quaternion.identity);
+		return null;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/simulation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simulation : MonoBehaviour {
+
+	public GameObject pitchUpMaxTransform;
+	public GameObject pitchDownMaxTransform;
+	float pitchDownMax;
+	float pitchUpMax;
+	bool isPitchingDown = true;
+	// Use this for initialization
+	void Start () {
+		pitchDownMax = Quaternion.Angle (transform.rotation,  pitchDownMaxTransform.transform.rotation);
+		pitchUpMax = Quaternion.Angle (transform.rotation, pitchUpMaxTransform.transform.rotation);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		pitchDownMax = Quaternion.Angle (transform.rotation,  pitchDownMaxTransform.transform.rotation);
+		pitchUpMax = Quaternion.Angle (transform.rotation, pitchUpMaxTransform.transform.rotation);
+		if (isPitchingDown) {
+			Debug.Log ("Pitching Down. Rotation : pitchDownMax" + pitchDownMax + " current pitch : ");
+			if (pitchDownMax > 2) {
+				// Rotate the object around its local X axis at 10 degrees per second
+				transform.Rotate (Vector3.right * (Time.deltaTime * 10), Space.World);
+			} else {
+				// Go the other way.
+				isPitchingDown = false;
+			}
+		} else {
+			if (pitchUpMax > 1) {
+				// Rotate the object around its local X axis at 10 degrees per second
+				transform.Rotate (Vector3.left * (Time.deltaTime * 10), Space.World);
+			} else {
+				// Go the other way.
+				isPitchingDown = true;
+			}
+		}
+	}
+}
