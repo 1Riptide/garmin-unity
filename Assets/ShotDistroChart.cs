0f9a95f05a22da73a55474672755446fac22f66a
Casey Stalnaker <casey@caseystalnaker.com>
2017-11-22 13:52:05 -0600

    Working back through git to ensure project can be shared. NOTE: I had to have both versions of unity on the same release in order to prevent problems. As of today, that is: Vs 2017.2.0f3

--- Assets/ShotDistroChart.cs
+++ Assets/ShotDistroChart.cs
@@ -3,30 +3,59 @@ using System.Collections;
 
 public class ShotDistroChart : MonoBehaviour {
 
-	public GameObject prefab;
+	public GameObject dataPoint;
+	public GameObject center;
+	// Light anim
+	public Light lt;
+	public float maxAngle;
+
+	private bool isCameraLocked = true;
+
+	// Speed of camera transition
+	float speed = .8f;
+	// Camera position when looking down at chart.
+	Vector3 topPosition = new Vector3(0,28,0);
 
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (addDataPoints ());
+		StartCoroutine (cameraAttitude ());
 	}
 
 	IEnumerator addDataPoints(){
-		int shotCount = 1490;
+		int shotCount = 750;
 		for (int i = 0; i < shotCount; i++) {
 			// x range is 8 thru -8
 			// y range is 14 thru -14
-			yield return new WaitForSeconds(Random.Range(0.01f, 0.04f));
+			yield return new WaitForSeconds(Random.Range(0.001f, 0.01f));
 			addDataPoint(new Vector3(Random.Range(-8.0f, 8.0f), 0, Random.Range(-14.0f, 14.0f)));
 		}
 	}
 
 	IEnumerator addDataPoint(Vector3 location){
-		//yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
-		Instantiate(prefab, location, Quaternion.identity);
+		Instantiate(dataPoint, location, Quaternion.identity);
 		return null;
 	}
+
+	IEnumerator cameraAttitude(){
+		yield return new WaitForSeconds (4);
+		isCameraLocked = false;
+
+
+	}
+		
 	// Update is called once per frame
 	void Update () {
-	
+		if (lt.spotAngle < maxAngle) {
+			lt.spotAngle += 5 * Time.deltaTime;
+		}
+
+		if (!isCameraLocked) {
+			Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, topPosition, speed * Time.deltaTime);
+		}
+	}
+
+	void LateUpdate() {
+		Camera.main.transform.LookAt (center.transform);
 	}
 }
