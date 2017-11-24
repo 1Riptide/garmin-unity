356b08e6285e41231d34887c779ac306a3794a98
Casey Stalnaker <casey@caseystalnaker.com>
2017-11-24 09:25:41 -0600

    11/24/17 Iterating. Added first 3DText elements. Updated textures.

--- Assets/ShotDistroChart.cs
+++ Assets/ShotDistroChart.cs
@@ -5,9 +5,16 @@ public class ShotDistroChart : MonoBehaviour {
 
 	public GameObject dataPoint;
 	public GameObject center;
+	public GameObject maxText;
+	public GameObject mainText;
+	public GameObject midText;
 	// Light anim
 	public Light lt;
 	public float maxAngle;
+	// min/max/mean
+	int maxY;
+	int minY;
+	int avgY;
 
 	private bool isCameraLocked = true;
 
@@ -19,7 +26,7 @@ public class ShotDistroChart : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (addDataPoints ());
-		StartCoroutine (cameraAttitude ());
+		StartCoroutine (cameraAttitude());
 	}
 
 	IEnumerator addDataPoints(){
@@ -27,7 +34,7 @@ public class ShotDistroChart : MonoBehaviour {
 		for (int i = 0; i < shotCount; i++) {
 			// x range is 8 thru -8
 			// y range is 14 thru -14
-			yield return new WaitForSeconds(Random.Range(0.001f, 0.01f));
+			yield return new WaitForSeconds(0);
 			addDataPoint(new Vector3(Random.Range(-8.0f, 8.0f), 0, Random.Range(-14.0f, 14.0f)));
 		}
 	}
@@ -40,19 +47,27 @@ public class ShotDistroChart : MonoBehaviour {
 	IEnumerator cameraAttitude(){
 		yield return new WaitForSeconds (4);
 		isCameraLocked = false;
-
-
 	}
 		
-	// Update is called once per frame
 	void Update () {
-		if (lt.spotAngle < maxAngle) {
-			lt.spotAngle += 5 * Time.deltaTime;
-		}
-
 		if (!isCameraLocked) {
+			if (lt.spotAngle < maxAngle) {
+				lt.spotAngle += 5 * Time.deltaTime;
+			}
 			Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, topPosition, speed * Time.deltaTime);
 		}
+
+		// max text
+		// min text
+		// mid text
+	
+		maxText.transform.LookAt (Camera.main.transform.position);
+		Vector3 targetPostition = new Vector3( maxText.transform.position.x, 
+			Camera.main.transform.position.y, 
+			Camera.main.transform.position.z) ;
+		maxText.transform.LookAt(targetPostition);
+		maxText.transform.Rotate(Vector3.up - new Vector3(0,180,0));
+
 	}
 
 	void LateUpdate() {
