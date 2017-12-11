0a41efc31359aea93e2d2c417dbf4271f77cd95a
Casey Stalnaker <casey.stalnaker@garmin.com>
2017-12-11 15:29:16 -0600

    Draft 2.4

---
+++ Assets/ApproachChart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApproachChart : MonoBehaviour {
+
+	// Default shot object.
+	public GameObject dataPoint;
+	public static Stack<GameObject> dataPoints = new Stack<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
